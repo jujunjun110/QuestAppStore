3503548842685ddc47cc29b9cd18feb3638c418f
Jun Ito <jun_ito@voyagegroup.com>
2020-06-07 00:13:30 +0900

    Add: 完成

--- main.py
+++ main.py
@@ -4,26 +4,31 @@ import sys
 
 
 def main():
+    run(["mkdir -p apps".split(" ")])
     cmd = "adb shell pm list packages".split(" ")
-    packages = run(cmd, stdout=subprocess.PIPE).stdout.decode(
-        'utf-8').split("\n")
+    packages = run(cmd, stdout=subprocess.PIPE).stdout.decode("utf-8").split("\n")
 
-    developers = ["jujunjun110", "MESON"]
+    developers = ["jujunjun110", "MESON", "DefaultCompany", "Hancho"]
     target_packages = extract_target_packages(packages, developers)
-
     print(target_packages)
 
     for package in target_packages:
-        res = run(["adb", "shell", "pm", "path", package],
-                  stdout=subprocess.PIPE)
-        path = res.stdout.decode(
-            'utf-8').replace("package:", "").replace("\n", "")
-        res2 = run(["adb", "pull", path, f"./apps/{package}.apk"])
-        run(["adb", "uninstall", package])
+        pull_package(package)
+        uninstall_package(package)
 
 
 def extract_target_packages(packages, developers):
     return sum([[p.replace("package:", "") for p in packages if d in p] for d in developers], [])
+
+
+def pull_package(package):
+    res = run(["adb", "shell", "pm", "path", package], stdout=subprocess.PIPE)
+    path = res.stdout.decode("utf-8").replace("package:", "").replace("\n", "")
+    run(["adb", "pull", path, f"./apps/{package}.apk"])
+
+
+def uninstall_package(package):
+    run(["adb", "uninstall", package])
 
 
 if __name__ == "__main__":
